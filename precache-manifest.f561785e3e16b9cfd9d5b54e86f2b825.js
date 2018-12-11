1ca0e97e8dcf73b2c582c4cf00532e37c53cb8b9
hiimterence <xaloh112703@gmail.com>
2018-12-11 16:33:49 +0800

    Updates

---
+++ precache-manifest.f561785e3e16b9cfd9d5b54e86f2b825.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56df041e24f928e7c754",
+    "url": "/static/js/main.56df041e.chunk.js"
+  },
+  {
+    "revision": "936c4bb899d288672a74",
+    "url": "/static/js/1.936c4bb8.chunk.js"
+  },
+  {
+    "revision": "56df041e24f928e7c754",
+    "url": "/static/css/main.55509fa0.chunk.css"
+  },
+  {
+    "revision": "64da701ba4f961b110481fa875ffb04c",
+    "url": "/index.html"
+  }
+];
